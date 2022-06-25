b3b2778938212dab52366a0b65c065bdf59f7817
onwujekwechibuzor@gmail.com <onwujekwechibuzor@gmail.com>
2022-06-25 17:29:23 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const ify = ["f","r","e","e","c","o","d","e","c","a","m","p"] 
+ify.forEach(a => {
+if(a.length > 0){
+        alert(a.length - a.length[10])
+    }
+else  if (a.length == 0) {
+    alert(1)
+}
+
+}) 
+function name(params) {
+    
+}
+
